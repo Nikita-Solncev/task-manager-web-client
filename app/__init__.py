fa59f99dee71f46c508746440dc048146ebf2e92
Nikita Solncev <solncev.nikita1501@gmail.com>
2024-06-04 21:10:39 +1000

    всё плохо

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from .views import configure_routes
 
 
 def create_app():
     app = Flask(__name__)
+    cors = CORS(app, origins="*")
     configure_routes(app)
     
     return app
--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
 from flask import redirect, url_for, render_template, request
 from .forms import RegistrationForm, LoginForm
+# from app.services.api_client import APIClient
+
+
+# api = APIClient("http://localhost:5000")
 
 
 def configure_routes(app):
@@ -19,7 +23,8 @@ def configure_routes(app):
 
     @app.route("/login")
     def login():
-        form = LoginForm(request.form)
+        form = LoginForm(request.form)        
+        # api.post_data("/login", data={"username": form.username,"password": form.password})
         return render_template("login.html", form=form)
 
 
@@ -31,3 +36,9 @@ def configure_routes(app):
     @app.route("/project")
     def project():
         return render_template("project.html")
+    
+    
+    #HELP VIEWS
+    app.route("loginhelp")
+    def login_help():
+        return redirect(url_for("/home"))
